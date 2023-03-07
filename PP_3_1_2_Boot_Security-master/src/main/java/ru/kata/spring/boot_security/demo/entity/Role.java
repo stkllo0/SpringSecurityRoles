0d59c48300541bf79d687ee0cc89633d69dc131c
stkllo0 <106972900+stkllo0@users.noreply.github.com>
2023-03-07 18:59:11 +0300

    Update Role.java

--- PP_3_1_2_Boot_Security-master/src/main/java/ru/kata/spring/boot_security/demo/entity/Role.java
+++ PP_3_1_2_Boot_Security-master/src/main/java/ru/kata/spring/boot_security/demo/entity/Role.java
@@ -14,6 +14,9 @@ public class Role implements GrantedAuthority {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
+    
+    @ManyToMany(mappedBy = "roles")
+    private Collection<User> users;
 
     public Role() {
     }
@@ -37,11 +40,18 @@ public class Role implements GrantedAuthority {
         this.name = name;
     }
 
+    public Collection<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Collection<User> users) {
+        this.users = users;
+    }
+    
     @Override
     public String getAuthority() {
         return getName();
     }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
